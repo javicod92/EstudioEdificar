38af9ce88608b4432ac76fff2abc92b0c917f906
javicod92 <javi.javier.jc@gmail.com>
2023-01-28 23:50:05 -0300

    ultimas modificaciones
    ultimas modificaciones

--- static/js/proyectos/albumes.js
+++ static/js/proyectos/albumes.js
@@ -1,8 +1,8 @@
 let sliderSection = document.getElementsByClassName("album_contenedorFotos");
 const boton = document.getElementsByClassName('boton');
-const totalSlider = (sliderSection.length - 1);
+const totalSlider = (sliderSection.length);
 
-if (totalSlider){
+if (totalSlider >= 2){
     const slider = document.getElementById("album_contPpal"), btnLeft = document.getElementById("previous"), btnRight = document.getElementById("next");
     let sliderSectionLast = sliderSection[sliderSection.length - 1].cloneNode(true);
     slider.insertAdjacentElement("afterbegin", sliderSectionLast); 
@@ -44,3 +44,12 @@ if (totalSlider){
         boton[i].style.display = 'none';
     };
 };
+
+/* 
+Fundamentos del Javascript - en primer lugar el código se fija la cantidad de imágenes (albumes) existentes extraidos de la base de datos.
+Luego de ello si la cantidad es 0 o 1, deshabilita las teclas de avanzar y retroceder y deja que el slider tenga un width del 100%.
+Si los albumes son 2 o más, lo que se hace es mantener habilitadas las teclas de avanzar y retroceder así como tambien se posicionan
+las imágenes a la espera del evento de los botones. Utilizo cloneNode(true) para clonar el álbum y posicionarlo donde se lo requiera,
+ya que si no uso el clone, al hacer referencia a la misma dirección de memoria el albun solo se posiciona (no se copia).
+Por otro lado tambien calculo el width que debe tener el slider en funcion de la cantidad de álbumes existentes.
+*/
